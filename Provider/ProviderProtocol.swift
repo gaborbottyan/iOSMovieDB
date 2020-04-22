4a35669c62b6cc487a240a7a178ea19bc2ad1776
Sebastian Diaz Avila <diazavilasebastian@gmail.com>
2020-04-21 22:20:57 -0400

    Fix formater text

--- Provider/ProviderProtocol.swift
+++ Provider/ProviderProtocol.swift
@@ -6,14 +6,13 @@
 //
 
 import Foundation
-import Foundation
 
 protocol ProviderProtocol {
 
     typealias listMoviesResult = (Result<PageMovies>) -> Void
     typealias movieResult = (Result<Movie>) -> Void
 
-    func getMovie(id: Int, completition: @escaping movieResult )
+    func getMovie(id: Int, completition: @escaping movieResult)
     func getTopRated(completition: @escaping listMoviesResult)
     func getUpcoming(completition: @escaping listMoviesResult)
     func getPopular(page: Int, completition: @escaping listMoviesResult)
